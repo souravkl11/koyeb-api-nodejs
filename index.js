d421e9969cfead3e7da6462c83bfc1843f8e1ef0
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-12-20 12:17:41 +0530

    add pauseService & fix reDeploy, setEnv

--- index.js
+++ index.js
@@ -97,6 +97,29 @@ const reDeploy = async (appName, token) => {
         throw new Error(error.message)
     }
 }
+const pauseService = async (appName, token) => {
+    if (!appName) throw new Error('Need app name!')
+    if (!token) throw new Error('Koyeb API Key not found!')
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    };
+    try {
+        let axiosResponse0 = await axios.get(
+            'https://app.koyeb.com/v1/services',
+            config
+        );
+        const service_id = axiosResponse0.data.services.filter(e => e.name == appName.trim())[0].id
+         await axios.post(
+            `https://app.koyeb.com/v1/services/${service_id}/pause`,
+            config
+        );
+        return true;
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
 const update = async (key, value, token, appName) => {
     if (!key || !value) throw new Error('Need key & value!')
     if (!token) throw new Error('Koyeb API Key not found!')
@@ -119,7 +142,7 @@ const update = async (key, value, token, appName) => {
             definition
         } = axiosResponse1.data.deployments[0]
         let envVars = axiosResponse1.data.deployments[0]?.definition?.env || []
-        let matched = env.filter(e => e.key === key)
+        let matched = envVars.filter(e => e.key === key)
         if (matched.length) {
             if (envVars[envVars.indexOf(matched[0])]['value'] == value) return true;
             envVars[envVars.indexOf(matched[0])]['value'] = value
@@ -146,22 +169,23 @@ module.exports = class Koyeb {
         if (!token) throw "API token not found!"
         /** Add/update environment variables. All params are required
          * ```
-         * Koyeb.setEnv("PORT","3000","my-app")
+         * Koyeb.setEnv({Var:"PORT",value:"3000",serviceName:"my-service-name"})
          * ``` */
         this.setEnv = ({
             Var,
             value,
-            appName
-        }) => update(Var, value, token, appName);
-        /** Redeploy service using app name (required)
+            serviceName
+        }) => update(Var, value, token, serviceName);
+        /** Redeploy service using service name (required)
          * ```
-         * Koyeb.reDeploy("my-app")
+         * Koyeb.reDeploy("my-service")
          * ``` */
-        this.reDeploy = (appName) => reDeploy(appName, token)
+        this.reDeploy = (serviceName) => reDeploy(serviceName, token)
         this.getApps = () => getApps(token)
         this.getServices = () => getServices(token)
         this.listActivities = () => listActivities(token)
         this.getAppInfo = (appName) => getAppInfo(appName, token)
         this.getServiceInfo = (service_id) => getServiceInfo(service_id, token)
+        this.pauseService = (serviceName) => pauseService(serviceName,token)
     }
 }
